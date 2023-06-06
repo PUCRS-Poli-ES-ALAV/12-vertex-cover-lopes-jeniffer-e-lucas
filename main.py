3a4b5d9c10d193aa0a7e9eac152e06a196aae403
Gustavo Beche Lopes <gustavo.beche@gmail.com>
2023-06-06 18:28:19 -0300

    Funciona?

--- main.py
+++ main.py
@@ -66,24 +66,78 @@ class Graph:
                         if neighbor not in visited:
                             queue.append(neighbor)
     
-    def CVA(g):
-        c = None
-        e = g * e
+    def calcVertexCover(self):
+        """
+        Calcula a cobertura de vértices do grafo.
+        """
+        # Inicializa a cobertura de vértices
+        vertexCover = set()
+        copy = self.graph.copy()
 
-        while (e != None):
+        # Enquanto houverem arestas no grafo
+        while copy:
+            # Escolhe uma aresta qualquer
+            # edge = next(iter(copy))
+            # print(copy)
+            edge = copy.popitem()
 
+            if not edge[1]:
+                continue
+
+            # Adiciona os vértices da aresta na cobertura
+            # vertexCover.add(edge)
+            # print(edge)
+            vertexCover.add(edge[0] + edge[1][0])
+            # for i in edge[1]:
+            #     vertexCover.add(edge[0] + i)
+            #vertexCover.add(edge[1])
+
+            # Remove todas as arestas que contém os vértices da aresta escolhida
+            # copy = {k: v for k, v in copy.items() if edge[0] not in v and edge[1] not in v}
+            # copy = {k: v for k, v in copy.items() if edge not in v}
+            # copy.pop(edge[0])
+            # print(edge[0])
+            self._removeEdgesFromNode(copy, edge[0])
+            # self.removeEdges(copy, edge[1])
+
+        return vertexCover
+
+    def _removeEdgesFromNode(self, g, node):
+        """
+        Remove todas as arestas que contém o nó dado.
+
+        Args:
+            node: O nó que será removido das arestas.
+        """
+        if node in g:
+            g.pop(node)
+        for key, value in g.items():
+            if node in value:
+                value.remove(node)
 
 
 # Exemplo de uso
 g = Graph()
-g.add_edge(1, 2)
-g.add_edge(1, 3)
-g.add_edge(2, 3)
-g.add_edge(3, 4)
-g.add_edge(4, 2)
+# g.add_edge(1, 2)
+# g.add_edge(1, 3)
+# g.add_edge(2, 3)
+# g.add_edge(3, 4)
+# g.add_edge(4, 2)
 
-print("DFS:")
-g.dfs(1)
+g.add_edge("a", "b")
+g.add_edge("a", "c")
+g.add_edge("b", "c")
+g.add_edge("c", "d")
+g.add_edge("d", "b")
+g.add_edge("d", "e")
+g.add_edge("e", "d")
+g.add_edge("e", "a")
+g.add_edge("e", "f")
 
-print("BFS:")
-g.bfs(1)
+# print("DFS:")
+# g.dfs(1)
+
+# print("BFS:")
+# g.bfs(1)
+
+print(g.calcVertexCover())
